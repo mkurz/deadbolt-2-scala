495fc7d62c4d17d85aa7b8b691a2e43696fbaa46
Khandramay Pavel <khandramay@gmail.com>
2014-07-27 19:54:24 +0400

    fix for previous commit

--- app/be/objectify/deadbolt/scala/DeadboltActionBuilders.scala
+++ app/be/objectify/deadbolt/scala/DeadboltActionBuilders.scala
@@ -13,10 +13,6 @@ object DeadboltActionBuilders {
 
     def apply(roles: String*) = RestrictActionBuilder(roles:_*)
 
-    def async(roles: String*)(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = RestrictActionBuilder(roles: _*).async(block)(deadbloltHandler)
-    def async(roles: String*)(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = RestrictActionBuilder(roles: _*).async(block)(deadbloltHandler)
-    def async[A](roles: String*)(bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = RestrictActionBuilder(roles: _*).async(bodyParser)(block)(deadbloltHandler)
-
     case class RestrictActionBuilder(roles: String*) extends DeadboltActionBuilder {
 
       override def async[A](bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadboltHandler: DeadboltHandler) : Action[A] =
@@ -28,14 +24,6 @@ object DeadboltActionBuilders {
   object DynamicAction extends DeadboltActions {
 
     def apply(name: String, meta: String = "") = DynamicActionBuilder(name, meta)
-
-    def async(name: String)(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler):Action[AnyContent] = DynamicActionBuilder(name, "").async(block)(deadbloltHandler)
-    def async(name: String)(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = DynamicActionBuilder(name, "").async(block)(deadbloltHandler)
-    def async[A](name: String)(bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = DynamicActionBuilder(name, "").async(bodyParser)(block)(deadbloltHandler)
-
-    def async(name: String, meta: String)(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = DynamicActionBuilder(name, meta).async(block)(deadbloltHandler)
-    def async(name: String, meta: String)(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = DynamicActionBuilder(name, meta).async(block)(deadbloltHandler)
-    def async[A](name: String, meta: String)(bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = DynamicActionBuilder(name, meta).async(bodyParser)(block)(deadbloltHandler)
 
     case class DynamicActionBuilder(name: String, meta: String = "") extends DeadboltActionBuilder {
 
@@ -49,10 +37,6 @@ object DeadboltActionBuilders {
 
     def apply(value: String, patternType: PatternType) = PatternActionBuilder(value, patternType)
 
-    def async(value: String, patternType: PatternType)(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = PatternActionBuilder(value, patternType).async(block)(deadbloltHandler)
-    def async(value: String, patternType: PatternType)(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = PatternActionBuilder(value, patternType).async(block)(deadbloltHandler)
-    def async[A](value: String, patternType: PatternType)(bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = PatternActionBuilder(value, patternType).async(bodyParser)(block)(deadbloltHandler)
-
     case class PatternActionBuilder(value: String, patternType: PatternType) extends DeadboltActionBuilder {
 
       override def async[A](bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadboltHandler: DeadboltHandler) : Action[A] =
@@ -65,10 +49,6 @@ object DeadboltActionBuilders {
 
     def apply = SubjectPresentActionBuilder()
 
-    def async(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = SubjectPresentActionBuilder().async(block)(deadbloltHandler)
-    def async(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = SubjectPresentActionBuilder().async(block)(deadbloltHandler)
-    def async[A](bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = SubjectPresentActionBuilder().async(bodyParser)(block)(deadbloltHandler)
-
     case class SubjectPresentActionBuilder() extends DeadboltActionBuilder {
 
       override def async[A](bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadboltHandler: DeadboltHandler) : Action[A] =
@@ -80,10 +60,6 @@ object DeadboltActionBuilders {
   object SubjectNotPresentAction extends DeadboltActions {
 
     def apply = SubjectNotPresentActionBuilder()
-
-    def async(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = SubjectNotPresentActionBuilder().async(block)(deadbloltHandler)
-    def async(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = SubjectNotPresentActionBuilder().async(block)(deadbloltHandler)
-    def async[A](bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadbloltHandler: DeadboltHandler) = SubjectNotPresentActionBuilder().async(bodyParser)(block)(deadbloltHandler)
 
     case class SubjectNotPresentActionBuilder() extends DeadboltActionBuilder {
 
@@ -101,9 +77,9 @@ object DeadboltActionBuilders {
       Future.successful(block(req))
     }
 
-    private[DeadboltActionBuilders] def async(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler): Action[AnyContent] = async( _ => block)(deadbloltHandler)
-    private[DeadboltActionBuilders] def async(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadboltHandler: DeadboltHandler): Action[AnyContent] = async(BodyParsers.parse.anyContent)(block)(deadboltHandler)
-    private[DeadboltActionBuilders] def async[A](bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadboltHandler: DeadboltHandler): Action[A]
+    def async(block: => Future[Result])(implicit deadbloltHandler: DeadboltHandler): Action[AnyContent] = async( _ => block)(deadbloltHandler)
+    def async(block: AuthenticatedRequest[AnyContent] => Future[Result])(implicit deadboltHandler: DeadboltHandler): Action[AnyContent] = async(BodyParsers.parse.anyContent)(block)(deadboltHandler)
+    def async[A](bodyParser: BodyParser[A])(block: AuthenticatedRequest[A] => Future[Result])(implicit deadboltHandler: DeadboltHandler): Action[A]
 
     def withHandler(deadboltHandler: DeadboltHandler) = new {
       def apply(block: => Result): Action[AnyContent] =
