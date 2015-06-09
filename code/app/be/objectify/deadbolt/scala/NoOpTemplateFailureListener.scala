acc1668afec59c9ac7dbeb24dec58ae4c744b91a
Steve Chaloner <bitbucket@objectify.be>
2015-06-09 07:47:52 +0200

    no-op template failure listener now really is no-op

--- code/app/be/objectify/deadbolt/scala/NoOpTemplateFailureListener.scala
+++ code/app/be/objectify/deadbolt/scala/NoOpTemplateFailureListener.scala
@@ -2,17 +2,13 @@ package be.objectify.deadbolt.scala
 
 import javax.inject.Singleton
 
-import play.api.Logger
-
 /**
- * No-op (or pretty close to it) implementation of TemplateFailureListener.  It trace-logs the received information, and nothing else.
+ * No-op implementation of TemplateFailureListener..
  *
  * @author Steve Chaloner (steve@objectify.be)
  */
 @Singleton
 class NoOpTemplateFailureListener extends TemplateFailureListener {
 
-  val logger: Logger = Logger("deadbolt.template")
-
-  override def failure(message: String, timeout: Long): Unit = if (logger.isTraceEnabled) logger.trace(s"Template constraint failure notification:  Message [$message]  timeout [$timeout]")
+  override def failure(message: String, timeout: Long): Unit = {}
 }
