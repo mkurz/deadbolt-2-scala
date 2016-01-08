dfba0296fed2579ed827f0e87547e9689d52f7cc
Steve Chaloner <bitbucket@objectify.be>
2016-01-08 13:14:02 +0100

    updated copyright year to 2016

--- code/app/be/objectify/deadbolt/scala/DynamicResourceHandler.scala
+++ code/app/be/objectify/deadbolt/scala/DynamicResourceHandler.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 Steve Chaloner
+ * Copyright 2012-2016 Steve Chaloner
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
